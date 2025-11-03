ec4928ed98cbcfc0464b03c7bb98e65feb0408f5
Noman <nomana@localhost.localdomain>
2025-11-04 00:02:36 +0600

    loading spinner has been added

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+function LoadingSpinner() {
+  return (
+    <div className="flex justify-center items-center py-16">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-600 border-b-4 border-gray-200"></div>
+    </div>
+  )
+}
+
+export default LoadingSpinner
